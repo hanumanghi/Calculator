b99c4c3f928a0616015897240e21276fdafe0cbc
hanumanghi <drewpatel97@gmail.com>
2022-08-04 17:17:03 -0500

    Number is displayed when selected

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const operators = document.querySelectorAll(".operators");
+const numbers = document.querySelectorAll(".numbers");
+const display = document.querySelector(".display");
+
+operators.forEach(operator => operator.addEventListener('click',operate));
+numbers.forEach(number =>  number.addEventListener('click',show))
+
+function show(e){
+    let numberSelection  = e.target.id;
+    
+    display.textContent = numberSelection;
+}
 function add (x,y){
    return x + y;
 }
@@ -14,16 +26,24 @@ function divide(x,y){
     return x / y;
 }
 
-function operate(){
-    let x = 0;
-    let y = 0;
-    if(operator === '+'){
-        return add(x,y);
-    } else if(operator === '-'){
-        return subtract(x,y);
-    } else if(operator === '*'){
-        return multiply(x,y);
-    } else if(operator === '/'){
-        return divide(x,y);
+// let x = a string and send that string through a loop and
+//like the fibonacci seq for multiply
+function operate(e){
+    let operation = e.target.id;
+    let x = 12;
+    let y = 4;
+    
+    if(operation == '+'){
+        let answer = add(x,y);
+        console.log(answer);
+    } else if(operation == '-'){
+        let answer = subtract(x,y);
+        console.log(answer);
+    } else if(operation == '*'){
+        let answer = multiply(x,y);
+        console.log(answer);
+    } else if(operation == '/'){
+        let answer = divide(x,y);
+        console.log(answer);
     }
 }
