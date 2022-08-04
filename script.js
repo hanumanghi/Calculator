40c9631781010f8d93759d1d313fe221f30ccb37
hanumanghi <drewpatel97@gmail.com>
2022-08-04 15:26:15 -0500

    added functions add, subtract,divide and multiply along with an operator.

---
+++ script.js
@@ -0,0 +1,29 @@
+function add (x,y){
+   return x + y;
+}
+
+function subtract(x,y){
+    return x - y;
+}
+
+function multiply(x,y){
+    return x * y;
+}
+
+function divide(x,y){
+    return x / y;
+}
+
+function operate(){
+    let x = 0;
+    let y = 0;
+    if(operator === '+'){
+        return add(x,y);
+    } else if(operator === '-'){
+        return subtract(x,y);
+    } else if(operator === '*'){
+        return multiply(x,y);
+    } else if(operator === '/'){
+        return divide(x,y);
+    }
+}
