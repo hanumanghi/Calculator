6123cab077b5e4567af4fbdc99886f426fe8f082
hanumanghi <drewpatel97@gmail.com>
2022-08-10 13:51:57 -0500

    Added function so screen display clears once an operator is chosen

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 const operators = document.querySelectorAll(".operators");
 const numbers = document.querySelectorAll(".numbers");
 const display = document.querySelector(".display");
+const equal = document.getElementById("=")
 
-operators.forEach(operator => operator.addEventListener('click',operate));
-numbers.forEach(number =>  number.addEventListener('click',show))
+operators.forEach(operator => operator.addEventListener('click', show))
+numbers.forEach(number =>  number.addEventListener('click',numChoice))
+
+let flag = false;
+let numberSelection;
+
+function numChoice(e){
+    if(flag == false){
+        numberSelection = e.target.id;
+        flag = true;
+    }else {
+       numberSelection = numberSelection  + e.target.id;
+    }
+    display.textContent = numberSelection;
+  // numberSele = string so figure out how to splice before and after operate
+  //might have to store operator under different function
+    }
+
 
 function show(e){
-    let numberSelection  = e.target.id;
-    
-    display.textContent = numberSelection;
+    display.innerHTML = "";
+    let numOne = numberSelection;
+    let operation = e.target.id;
+    console.log(numOne);
+    console.log(operation);
 }
+
 function add (x,y){
    return x + y;
 }
@@ -26,24 +46,29 @@ function divide(x,y){
     return x / y;
 }
 
+
 // let x = a string and send that string through a loop and
 //like the fibonacci seq for multiply
 function operate(e){
-    let operation = e.target.id;
-    let x = 12;
-    let y = 4;
-    
-    if(operation == '+'){
-        let answer = add(x,y);
-        console.log(answer);
+   
+for(let i = 0; i < operation.length; i++)
+  {  if(operation == '+'){
+        let answerAdd = add(x,y);
+        console.log(answerAdd);
     } else if(operation == '-'){
-        let answer = subtract(x,y);
-        console.log(answer);
+        let answerSub = subtract(x,y);
+        console.log(answerSub);
     } else if(operation == '*'){
-        let answer = multiply(x,y);
-        console.log(answer);
+        let answerMulti = multiply(x,y);
+        console.log(answerMulti);
     } else if(operation == '/'){
-        let answer = divide(x,y);
-        console.log(answer);
+        let answerDivide = divide(x,y);
+        console.log(answerDivide);
+    }
     }
 }
+document.getElementById('clear').addEventListener('click', refresh)
+function refresh(){
+    window.location.reload(true)
+}
+
